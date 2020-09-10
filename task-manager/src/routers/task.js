9fc077b51d1efcacc8126bc34f8f043f6c1b19d1
DwanW <kunvang@yahoo.com>
2020-09-09 20:06:56 -0600

    added router, login, logout route, document schema, authentication and password encryption

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -46,7 +46,10 @@ router.patch('/task/:id', async (req, res) => {
         return res.status(400).send({ error: 'Invalid Operation'})
     }
     try {
-        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        const task = await Task.findById(_id)
+        updates.forEach((update) => task[update] = res.body[update])
+        await task.save()
+        // const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
         if(!task) {
             return res.status(404).send()
         }
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
+const auth = require('../middleware/auth')
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (error) {
-        res.status(500).send()
-    }
+router.get('/users/profile', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/user/:id', async (req, res) => {
@@ -24,11 +20,43 @@ router.get('/user/:id', async (req, res) => {
     }
 })
 
+router.post('/users/login', async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.findByCredentials(email, password)
+        const token = await user.generateAuthToken()
+        res.send({user, token})
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
 router.post('/users', async (req, res) => {
     const newUser = new User(req.body)
     try {
         await newUser.save()
-        res.status(201).send(newUser)
+        const token = await newUser.generateAuthToken()
+        res.status(201).send({newUser, token})
     } catch (error) {
         res.status(400).send(error)
     }
@@ -42,12 +70,15 @@ router.patch('/user/:id', async (req, res) => {
         return allowedUpdates.includes(update)
     })
 
-    if (!isValidOperation){
-        return res.status(400).send({ error: 'Invalid Operation'})
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid Operation' })
     }
     try {
-        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
-        if(!user) {
+        const user = await User.findById(_id)
+        updates.forEach((update) => user[update] = res.body[update])
+        await user.save()
+        // const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        if (!user) {
             return res.status(404).send()
         }
         res.send(user)
@@ -60,7 +91,7 @@ router.delete('/user/:id', async (req, res) => {
     const _id = req.params.id
     try {
         const user = await User.findByIdAndDelete(_id)
-        if(!user){
+        if (!user) {
             return res.status(404).send()
         }
         res.send(user)
